1c7437227ab845ded22e4c310b812be01c3d6f32
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-09 17:25:01 +0000

    Great 👍 You already have your MK SERVICE logo image. Now I’ll show you

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,8 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
         />
+        {/* Favicon (Browser Tab Icon) */}
+        <link rel="icon" href="https://i.ibb.co/C0W21mP/logo-192.png" type="image/png" />
       </head>
       <body className="font-body antialiased">
         {children}
