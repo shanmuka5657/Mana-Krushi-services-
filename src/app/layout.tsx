c796d766c081e144f080cdad09cb49acb06f47c0
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 17:47:59 +0000

    Again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,22 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cuelinksScript = `
+    var cId = '245924';
+    (function(d, t) {
+      var s = document.createElement('script');
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv2.js';
+      document.getElementsByTagName('body')[0].appendChild(s);
+    }());
+  `;
+
   return (
     <html lang="en">
       <body>
         {children}
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: cuelinksScript }} />
       </body>
     </html>
   );
