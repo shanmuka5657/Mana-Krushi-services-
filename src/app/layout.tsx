2cea155504e5c3d435d05daf858e091f35f3fec3
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 21:43:02 +0000

    <script type="text/javascript">     var cId =  "245979";
        (function(

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,28 +6,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const cuelinksScript = `
-    var cId = '245924';
-    var cuelinksRun = [];
-    window.runCuelinks = function() {
-      if (typeof window.cuelinks?.js?.run === 'function') {
-        window.cuelinks.js.run();
-      } else {
-        cuelinksRun.push(1);
-      }
-    };
+    var cId =  "245979";
     (function(d, t) {
-      var s = document.createElement('script');
-      s.type = 'text/javascript';
+      var s = document.createElement("script");
+      s.type = "text/javascript";
       s.async = true;
-      s.src = (document.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv2.js';
-      s.onload = function() {
-        if (cuelinksRun.length > 0) {
-           if (typeof window.cuelinks?.js?.run === 'function') {
-             window.cuelinks.js.run();
-           }
-        }
-      };
-      document.getElementsByTagName('body')[0].appendChild(s);
+      s.src = (document.location.protocol == "https:" ? "https://cdn0.cuelinks.com/js/" : "http://cdn0.cuelinks.com/js/")  + "cuelinksv2.js";
+      document.getElementsByTagName("body")[0].appendChild(s);
     }());
   `;
 
