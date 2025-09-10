f1625580a7597dbe952c59cbcff04aac5caabb89
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 17:36:17 +0000

    <script type='text/javascript'>       var cId = '245924';
          (functi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,16 +39,17 @@ export default function RootLayout({
         </Script>
         <Script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js" strategy="afterInteractive"></Script>
         <script
+          type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
               var cId = '245924';
               (function(d, t) {
-                var s = d.createElement(t);
+                var s = d.createElement('script');
                 s.type = 'text/javascript';
                 s.async = true;
-                s.src = (d.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/') + 'cuelinksv2.js';
+                s.src = (d.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv2.js';
                 d.getElementsByTagName('body')[0].appendChild(s);
-              }(document, 'script'));
+              }());
             `,
           }}
         />
