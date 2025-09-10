1f254015a6506207db96830510adc3b30acc12b1
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 15:28:37 +0000

    Remove tubi also not available in india

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -15,9 +15,7 @@ import type { MovieSite } from '@/lib/types';
 const freeSites = [
     { name: 'YouTube', icon: <Clapperboard className="h-10 w-10 text-red-600" />, href: 'https://www.youtube.com/movies', color: 'bg-red-50' },
     { name: 'MX Player', icon: <PlayCircle className="h-10 w-10 text-blue-500" />, href: 'https://www.mxplayer.in/', color: 'bg-blue-50' },
-    { name: 'Hotstar', icon: <Tv className="h-10 w-10 text-blue-800" />, href: 'https://www.hotstar.com/in/explore', color: 'bg-blue-50' },
     { name: 'Plex', icon: <Tv className="h-10 w-10 text-green-500" />, href: 'https://www.plex.tv/watch-free', color: 'bg-green-50' },
-    { name: 'Tubi', icon: <Tv className="h-10 w-10 text-orange-500" />, href: 'https://tubitv.com/', color: 'bg-orange-50' },
 ];
 
 function MoviesPageContent() {
