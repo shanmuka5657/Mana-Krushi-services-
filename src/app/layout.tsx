b490cea2a4dc328f6e24c6d6ffbc9c51f4e3d948
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 17:46:04 +0000

    Same issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,21 +7,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <script
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var cId = '245924';
-              (function(d, t) {
-                var s = d.createElement('script');
-                s.type = 'text/javascript';
-                s.async = true;
-                s.src = (d.location.protocol == 'https:' ? 'https://cdn0.cuelinks.com/js/' : 'http://cdn0.cuelinks.com/js/')  + 'cuelinksv2.js';
-                d.getElementsByTagName('body')[0].appendChild(s);
-              }());
-            `,
-          }}
-        />
       </body>
     </html>
   );
