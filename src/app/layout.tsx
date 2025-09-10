d0c0a2f559db3b707b3fac4e4a9de1cef8cf9fdc
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-10 17:44:26 +0000

    Check this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,12 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "@/components/ui/alert-dialog";
-import Script from "next/script";
-
-export const metadata: Metadata = {
-  title: "Mana Krushi Services",
-  description: "Travel Agent Management System",
-  manifest: "/manifest.json",
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-        {/* Favicon (Browser Tab Icon) */}
-        <link rel="icon" href="https://i.ibb.co/LdbdX3Dp/file-00000000dad0622f92ca201d38c47e43.png" type="image/png" />
-      </head>
+    <html lang="en">
       <body>
         {children}
-        <Toaster />
         <script
           type="text/javascript"
           dangerouslySetInnerHTML={{
