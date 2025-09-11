d1e7840ded04a161c7873e9b00573c661bbfd655
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-11 13:12:09 +0000

    why auto ads is diplaying and it is sexual content

--- src/app/ads/page.tsx
+++ src/app/ads/page.tsx
@@ -72,16 +72,6 @@ function AdsPageContent() {
                     </CardContent>
                 </Card>
 
-                 {/* Social Bar Ad */}
-                <Card>
-                    <CardHeader>
-                        <CardTitle>Social Bar Ad</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                       <Script type='text/javascript' src='//exportseats.com/18/6e/e8/186ee8d3ed422373beaa184909e3545c.js' strategy="lazyOnload" />
-                    </CardContent>
-                </Card>
-
             </div>
         </AppLayout>
     );
