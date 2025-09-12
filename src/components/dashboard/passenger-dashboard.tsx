edde943ff5c7216d4ff4879f6ffa3b403821c29c
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-12 16:36:56 +0000

    Not working

--- src/components/dashboard/passenger-dashboard.tsx
+++ src/components/dashboard/passenger-dashboard.tsx
@@ -244,7 +244,7 @@ export default function PassengerDashboard({ onSwitchTab }: PassengerDashboardPr
                             )}
                         />
                     </div>
-                    <Button type="submit" className="w-full md:w-auto">
+                     <Button type="submit" className="w-full md:w-auto">
                         <Search className="mr-2 h-4 w-4" />
                         Search Rides
                     </Button>
