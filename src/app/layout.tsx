32a75013bb5b74f12c47597321455094b54ee7d6
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-17 21:56:54 +0000

    Installation error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,9 +15,13 @@ export default function RootLayout({
       <body>
         {children}
         <Toaster />
-        <Script id="al5sm-ad-script" strategy="afterInteractive">
-          {`(s=>{s.dataset.zone='9892058',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`}
-        </Script>
+        <script
+          data-cfasync="false"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(s=>{s.dataset.zone='9892058',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
+          }}
+        ></script>
       </body>
     </html>
   );
