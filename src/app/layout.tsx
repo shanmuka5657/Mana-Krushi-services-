ed27aefecbd1b545f83d3f04b102e8a89f6b0d26
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-17 22:00:36 +0000

    Same error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import Script from 'next/script';
+import PopunderAd from '@/components/ads/popunder-ad';
 
 export default function RootLayout({
   children,
@@ -13,13 +14,7 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
-        <script
-          data-cfasync="false"
-          type="text/javascript"
-          dangerouslySetInnerHTML={{
-            __html: `(s=>{s.dataset.zone='9892058',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`,
-          }}
-        ></script>
+        <PopunderAd />
         {children}
         <Toaster />
       </body>
