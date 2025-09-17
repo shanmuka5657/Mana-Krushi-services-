df2fb7eba0c2faf3426d71be6b872dd6f0a2f36d
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-17 22:17:14 +0000

    (function(O){!function(e){var t=O.Z();function n(r){if(t[r])return t[r][

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,19 @@ export default function RootLayout({
       <body>
         {children}
         <Toaster />
+        <Script id="service-worker-registration">
+          {`
+            if ('serviceWorker' in navigator) {
+              window.addEventListener('load', () => {
+                navigator.serviceWorker.register('/sw.js').then(registration => {
+                  console.log('SW registered: ', registration);
+                }).catch(registrationError => {
+                  console.log('SW registration failed: ', registrationError);
+                });
+              });
+            }
+          `}
+        </Script>
       </body>
     </html>
   );
