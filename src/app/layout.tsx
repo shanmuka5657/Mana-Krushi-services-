d98a7686e5ca5647ed48c93a06e2a37e5687e484
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-17 22:07:29 +0000

    Same error <script>(s=>{s.dataset.zone='9892058',s.src='https://al5sm.co

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import Script from 'next/script';
-import PopunderAd from '@/components/ads/popunder-ad';
 
 export default function RootLayout({
   children,
@@ -14,7 +13,13 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body>
-        <PopunderAd />
+        <script
+          data-cfasync="false"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(s=>{s.dataset.zone='9892058',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+          }}
+        ></script>
         {children}
         <Toaster />
       </body>
