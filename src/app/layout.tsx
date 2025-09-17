6ec13dff2c7ded50f79d788442eb49929219d3c5
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-17 19:58:34 +0000

    <script async custom-element="amp-auto-ads"         src="https://cdn.amp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async custom-element="amp-auto-ads"
+          src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+        </script>
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4057664444308456"
