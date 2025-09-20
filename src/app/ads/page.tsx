d746bae4a242b61a1be495d76b9871273fb034d1
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-20 14:14:15 +0000

    <script>(function(s){s.dataset.zone='9892058',s.src='https://al5sm.com/t

--- src/app/ads/page.tsx
+++ src/app/ads/page.tsx
@@ -8,16 +8,25 @@ import { MonitorPlay } from 'lucide-react';
 
 function AdsPageContent() {
     useEffect(() => {
-        const script = document.createElement('script');
-        script.innerHTML = `(function(s){s.dataset.zone='9892027',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
-        document.body.appendChild(script);
+        // First ad script
+        const script1 = document.createElement('script');
+        script1.innerHTML = `(function(s){s.dataset.zone='9892027',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
+        document.body.appendChild(script1);
+
+        // Second ad script
+        const script2 = document.createElement('script');
+        script2.innerHTML = `(function(s){s.dataset.zone='9892058',s.src='https://al5sm.com/tag.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`;
+        document.body.appendChild(script2);
 
         return () => {
-            // Attempt to clean up if the component unmounts, though this specific script might not be easily removable.
-            // This is best-effort cleanup.
-            const adScript = document.querySelector('script[src="https://groleegni.net/vignette.min.js"]');
-            if (adScript) {
-                document.body.removeChild(adScript);
+            // Attempt to clean up if the component unmounts. This is best-effort.
+            const adScript1 = document.querySelector('script[src="https://groleegni.net/vignette.min.js"]');
+            if (adScript1) {
+                // The script removes itself, but we can try to clean up the container if it's identifiable.
+            }
+            const adScript2 = document.querySelector('script[src="https://al5sm.com/tag.min.js"]');
+             if (adScript2) {
+                // The script removes itself, but we can try to clean up the container if it's identifiable.
             }
         }
     }, []);
