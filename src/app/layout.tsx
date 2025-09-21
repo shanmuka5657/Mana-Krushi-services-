0d6605e55fd2e2957fa348da155731a24abbdcad
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-21 12:38:56 +0000

    The ads are not running

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import * as React from 'react';
 import { logVisit } from '@/lib/storage';
 import dynamic from 'next/dynamic';
 import { usePathname } from 'next/navigation';
+import Script from 'next/script';
 
 const ConditionalFooter = dynamic(
   () => import('@/components/layout/conditional-footer'),
@@ -25,32 +26,19 @@ export default function RootLayout({
     logVisit(pathname);
   }, [pathname]);
 
-  React.useEffect(() => {
-    const script1 = document.createElement('script');
-    script1.type = 'text/javascript';
-    script1.src = '//markswaitingrouge.com/8d/62/5f/8d625f6c0ee1cd63f181069e4d8bab94.js';
-    script1.async = true;
-    
-    document.body.appendChild(script1);
-
-    return () => {
-      if (document.body.contains(script1)) {
-        document.body.removeChild(script1);
-      }
-    };
-  }, []);
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+          <Script async src="//x7i0.com/tag.min.js" data-zone="9905911" data-cfasync="false" strategy="afterInteractive" />
       </head>
       <body>
         {children}
         <Toaster />
         <ConditionalFooter />
+        <Script type='text/javascript' src='//markswaitingrouge.com/8d/62/5f/8d625f6c0ee1cd63f181069e4d8bab94.js' strategy="afterInteractive" />
       </body>
     </html>
   );
