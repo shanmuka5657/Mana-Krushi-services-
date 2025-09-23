965f3d36d31a240faa31170c76c1600173a2d67c
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-23 11:49:32 +0000

    add all this

--- public/sw.js
+++ public/sw.js
@@ -1,63 +1,41 @@
-// A basic service worker for PWA offline functionality
+// This is the service worker file.
 
 const CACHE_NAME = 'mana-krushi-cache-v1';
 const urlsToCache = [
   '/',
+  '/dashboard?role=passenger',
+  '/dashboard?role=owner',
+  '/login',
+  '/signup',
   '/offline',
-  '/manifest.json',
-  '/globals.css',
-  // Add other critical assets here, e.g., logo
-  'https://i.ibb.co/mrqBwfds/IMG-20250920-WA0025.jpg'
+  '/globals.css'
+  // Add other important assets and pages here
 ];
 
-// Install event: opens a cache and adds the core files to it.
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Activate event: cleans up old caches.
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            console.log('Deleting old cache:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Fetch event: serves assets from cache if available, otherwise fetches from network.
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(response => {
         // Cache hit - return response
         if (response) {
           return response;
         }
 
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          (response) => {
+        return fetch(event.request).then(
+          response => {
             // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
               return response;
             }
 
@@ -65,21 +43,36 @@ self.addEventListener('fetch', (event) => {
             // and because we want the browser to consume the response
             // as well as the cache consuming the response, we need
             // to clone it so we have two streams.
-            const responseToCache = response.clone();
+            var responseToCache = response.clone();
 
             caches.open(CACHE_NAME)
-              .then((cache) => {
+              .then(cache => {
                 cache.put(event.request, responseToCache);
               });
 
             return response;
           }
-        );
-      })
-      .catch(() => {
-          // If the network request fails and there is no cache,
-          // return the offline page.
+        ).catch(() => {
+          // If the network fails, and there's no cache, show offline page.
           return caches.match('/offline');
+        });
       })
+    );
+});
+
+
+self.addEventListener('activate', event => {
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
