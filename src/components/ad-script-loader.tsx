4a6c112dfced42c4be4e5620da7f8bf19fe5ab87
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-23 05:28:26 +0000

    <script>(function(s){s.dataset.zone='9918780',s.src='https://groleegni.n

--- src/components/ad-script-loader.tsx
+++ src/components/ad-script-loader.tsx
@@ -9,18 +9,15 @@ const AdScriptLoader = () => {
 
   useEffect(() => {
     const checkInitialStateAndSubscribe = async () => {
-      // 1. Fetch the initial state when the component mounts
       const initialState = await getSetting('areAdsEnabled');
       setAreAdsEnabled(initialState || false);
 
-      // 2. Subscribe to subsequent changes
       const unsubscribe = onAdsEnabledChange(setAreAdsEnabled);
       return unsubscribe;
     };
 
     const unsubscribePromise = checkInitialStateAndSubscribe();
 
-    // Cleanup function
     return () => {
       unsubscribePromise.then(unsubscribe => {
         if (unsubscribe) {
@@ -31,43 +28,49 @@ const AdScriptLoader = () => {
   }, []);
 
   useEffect(() => {
-    const scriptId = 'monetag-ad-script';
-    
-    // Find any existing script
-    const existingScript = document.getElementById(scriptId);
+    const scriptConfigs = [
+      {
+        id: 'monetag-ad-script-1',
+        innerHTML: `(function(s){s.dataset.zone='9915521',s.src='https://al5sm.com/tag.min.js'})(document.body.appendChild(document.createElement('script')))`
+      },
+      {
+        id: 'monetag-ad-script-2',
+        innerHTML: `(function(s){s.dataset.zone='9918780',s.src='https://groleegni.net/vignette.min.js'})([document.documentElement, document.body].filter(Boolean).pop().appendChild(document.createElement('script')))`
+      }
+    ];
 
     if (areAdsEnabled) {
-      // If ads are enabled and the script doesn't exist, create and add it.
-      if (!existingScript) {
-        const s = document.createElement('script');
-        s.id = scriptId;
-        s.async = true;
-        s.innerHTML = `(function(s){s.dataset.zone='9915521',s.src='https://al5sm.com/tag.min.js'})(document.body.appendChild(document.createElement('script')))`;
-        document.body.appendChild(s);
-      }
+      scriptConfigs.forEach(config => {
+        if (!document.getElementById(config.id)) {
+          const s = document.createElement('script');
+          s.id = config.id;
+          s.async = true;
+          s.innerHTML = config.innerHTML;
+          document.body.appendChild(s);
+        }
+      });
     } else {
-      // If ads are disabled and the script exists, remove it.
-      if (existingScript) {
-        document.body.removeChild(existingScript);
-      }
+      scriptConfigs.forEach(config => {
+        const script = document.getElementById(config.id);
+        if (script) {
+          document.body.removeChild(script);
+        }
+      });
     }
-    
-    // The cleanup function for this effect will run if the component unmounts
-    // while ads are enabled, ensuring the script is removed.
+
     return () => {
-      // Check if ads were enabled to decide if cleanup is needed.
-      // This avoids trying to remove a script that wasn't there.
       if (areAdsEnabled) {
-         const scriptToRemove = document.getElementById(scriptId);
-         if (scriptToRemove && scriptToRemove.parentNode === document.body) {
-             document.body.removeChild(scriptToRemove);
-         }
+        scriptConfigs.forEach(config => {
+          const script = document.getElementById(config.id);
+          if (script && script.parentNode === document.body) {
+            document.body.removeChild(script);
+          }
+        });
       }
     };
-
   }, [areAdsEnabled]);
 
-  return null; // This component does not render anything itself.
+  return null;
 };
 
 export default AdScriptLoader;
