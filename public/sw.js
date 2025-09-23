4406866e0dbaea98cbd7abd56dd1144f5184e465
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-23 08:41:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- public/sw.js
+++ public/sw.js
@@ -1,78 +1,47 @@
-// This is the service worker file.
+// A basic service worker for caching assets
 
 const CACHE_NAME = 'mana-krushi-cache-v1';
 const urlsToCache = [
   '/',
-  '/offline',
-  '/styles/globals.css',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/offline'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
+      .then(cache => {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(response => {
         // Cache hit - return response
         if (response) {
           return response;
         }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        ).catch(() => {
+        return fetch(event.request).catch(() => {
           // If the network request fails, and it's a navigation request,
-          // serve the offline page.
+          // return the offline page.
           if (event.request.mode === 'navigate') {
             return caches.match('/offline');
           }
-          return;
         });
       })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
