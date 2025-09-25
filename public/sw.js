d8123b1461869ffd89070c39421658eddae62390
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-25 15:18:07 +0000

    Loss again

--- public/sw.js
+++ public/sw.js
@@ -1,57 +1,47 @@
-// Basic service worker for caching assets and enabling offline access.
+// This is the "Offline page" service worker
 
-const CACHE_NAME = 'mana-krushi-cache-v1';
-const urlsToCache = [
-  '/',
-  '/offline',
-  '/styles/globals.css',
-  // Add other critical assets here
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// Install event: open a cache and add the core assets to it.
-self.addEventListener('install', (event) => {
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "/offline";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
   );
 });
 
-// Activate event: clean up old caches.
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
 
-// Fetch event: serve assets from cache if available, otherwise fetch from network.
-// If the network fetch fails, serve the offline page.
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
         }
 
-        return fetch(event.request).catch(() => {
-          // If the request is for navigation, show the offline page.
-          if (event.request.mode === 'navigate') {
-            return caches.match('/offline');
-          }
-        });
-      })
-  );
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });
