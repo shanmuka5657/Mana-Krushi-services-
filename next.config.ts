6ea97313a7b9f743411d64aaa321541c03d40a17
Shanmuka Avula <shanmukaavula475@gmail.com>
2025-09-25 12:52:30 +0000

    {   "id": "analysis:9000-firebase-studio-1757177149716.cluster-va5f6x3wz

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,9 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  sw: 'sw.js',
+  fallbacks: {
+    document: '/offline',
+  },
   disable: process.env.NODE_ENV === 'development'
 });
 
